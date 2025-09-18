8ae596b08829e18e419a8b7d42200c92c426185a
hogogo7 <hogogo7@naver.com>
2025-09-18 16:28:14 +0900

    v1.1 pandas NaN

--- week04/week04_pandas.py
+++ week04/week04_pandas.py
@@ -1,17 +1,10 @@
-import pandas as pd
 import seaborn as sns
 
-def square(x):
-    return x ** 2
-def cube(x):
-    return x ** 3
-
 mpg = sns.load_dataset("mpg")
-df = pd.DataFrame([
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9]
-], columns=['A', 'B', 'C'], index=[1, 2, 3])
-print(df)
-print(df.apply(square))
-print(df.apply(cube))
+
+# print(mpg.isnull().sum())
+# print(mpg.isnull())
+# print(mpg[mpg['horsepower'].isnull() == True])
+# print(mpg[mpg['horsepower'].isnull()])
+mpg_nan = mpg[mpg['horsepower'].isnull()]
+print(mpg_nan[['mpg','horsepower','displacement', 'origin']])
